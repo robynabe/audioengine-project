1b487f76e85037808765a33244b33e527765bea6
robynabe <64428921+robynabe@users.noreply.github.com>
2020-12-02 08:53:48 -0800

    video close button fixed

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -39,6 +39,58 @@ $(document).ready(() => {
       prevArrow: '#left-arrow-banner'
     });  
 
+});
+
+const mqListener = window.matchMedia('(min-width: 450px)');
+
+// Add the listener to the MQL variable
+mqListener.addListener(function(e){ 
+    initSlick(true, e.matches, 600, $('.room-device'), slideSettings);     })
+
+
+const slideSettings = {
+    appendDots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 5,
+    slidesToScroll: 2,
+    nextArrow: '#right-arrow-device',
+    prevArrow: '#left-arrow-device',
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+            slidesToShow: 4
+            }
+        },
+        {
+          breakpoint: 800,
+          settings: {
+            slidesToShow: 3,
+          }
+        },
+        {
+            breakpoint: 450,
+            settings: "unslick"
+        }
+    ]
+};
+
+const windowWidth = $(window).width();
+
+initSlick(true, false, 450, $('.room-device'), slideSettings);
+
+
+function initSlick(onPageLoad = true, match = false, width, slideElement, slideSettings){
+    if(onPageLoad && windowWidth > width){
+        slideElement.slick(slideSettings);
+    }else if(match){
+        slideElement.slick(slideSettings);
+    }
+} 
+
+/*
+
 $('.shop-device .room-device').slick({
     appendDots: true,
     infinite: true,
@@ -99,8 +151,9 @@ $('.shop-device .room-device').slick({
         }
     ]
  });
+ */
     
-});
+
 
 
 
