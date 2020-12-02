654c51495e75f3710f70f149a41007ee463523fb
robynabe <64428921+robynabe@users.noreply.github.com>
2020-12-02 15:58:25 -0800

    Slick slideshow fully functioning

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -45,10 +45,10 @@ const mqListener = window.matchMedia('(min-width: 450px)');
 
 // Add the listener to the MQL variable
 mqListener.addListener(function(e){ 
-    initSlick(true, e.matches, 600, $('.room-device'), slideSettings);     })
+    initSlick(true, e.matches, 450, $('.room-device'), slideSettingsShop);     })
 
 
-const slideSettings = {
+const slideSettingsShop = {
     appendDots: true,
     infinite: true,
     speed: 1000,
@@ -78,80 +78,69 @@ const slideSettings = {
 
 const windowWidth = $(window).width();
 
-initSlick(true, false, 450, $('.room-device'), slideSettings);
+initSlick(true, false, 450, $('.room-device'), slideSettingsShop);
 
 
-function initSlick(onPageLoad = true, match = false, width, slideElement, slideSettings){
+function initSlick(onPageLoad = true, match = false, width, slideElement, slideSettingsShop ){
     if(onPageLoad && windowWidth > width){
-        slideElement.slick(slideSettings);
+        slideElement.slick(slideSettingsShop);
     }else if(match){
-        slideElement.slick(slideSettings);
+        slideElement.slick(slideSettingsShop);
     }
 } 
 
-/*
+const secondMql = window.matchMedia('(min-width: 500px)');
 
-$('.shop-device .room-device').slick({
+// Add the listener to the MQL variable
+secondMql.addListener(function(e){ 
+    initSlick(true, e.matches, 500, $('.magazines'), slideSettingsNews);     })
+
+
+const slideSettingsNews = {
     appendDots: true,
     infinite: true,
     speed: 1000,
     slidesToShow: 5,
-    slidesToScroll: 2,
-    nextArrow: '#right-arrow-device',
-    prevArrow: '#left-arrow-device',
+    nextArrow: '#right-arrow-news',
+    prevArrow: '#left-arrow-news',
     responsive: [
-        {
-            breakpoint: 1200,
-            settings: {
-            slidesToShow: 4
-            }
+      {
+        breakpoint: 1000,
+        settings: {
+        slidesToShow: 4
+        }
         },
         {
           breakpoint: 800,
           settings: {
-            slidesToShow: 3,
+          slidesToShow: 3
           }
-        },
-        {
-            breakpoint: 450,
-            settings: "unslick"
-        }
-    ]
- });
-
-    $('.in-the-news .magazines').slick({
-      appendDots: true,
-      infinite: true,
-      speed: 1000,
-      slidesToShow: 5,
-      nextArrow: '#right-arrow-news',
-      prevArrow: '#left-arrow-news',
-    responsive: [
-      {
-            breakpoint: 1000,
-            settings: {
-            slidesToShow: 4
-            }
-        },
-        {
-            breakpoint: 800,
-            settings: {
-            slidesToShow: 3
-            }
         },
         {
           breakpoint: 700,
           settings: {
-            slidesToShow: 2
+          slidesToShow: 2
           }
         },
         {
-            breakpoint: 500,
-            settings: "unslick"
+          breakpoint: 500,
+          settings: "unslick"
         }
     ]
- });
- */
+};
+
+const secondWindowWidth = $(window).width();
+
+initSlick(true, false, 500, $('.magazines'), slideSettingsNews);
+
+
+function initSlick(onPageLoad = true, match = false, width, slideElement, slideSettingsNews ){
+    if(onPageLoad && windowWidth > width){
+        slideElement.slick(slideSettingsNews);
+    }else if(match){
+        slideElement.slick(slideSettingsNews);
+    }
+} 
     
 
 
